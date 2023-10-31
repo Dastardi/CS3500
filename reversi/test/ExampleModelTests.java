1a269a3e1ad3ab1900062c6fac208a40b1b5d5d2
Luke Currier <lukescurrier@Gmail.com>
2023-10-31 16:06:38 -0400

    Made variable board size worked, changes getNeighbors to use try-catch rather than the old method (which was throwing for some reason, and more

--- reversi/test/ExampleModelTests.java
+++ reversi/test/ExampleModelTests.java
@@ -11,58 +11,58 @@ import org.junit.Test;
  * Holds example tests to illustrate the main functionality of the model.
  */
 public class ExampleModelTests {
-    ReversiModel model;
+  ReversiModel model;
 
-    @Before
-    public void init() {
-        this.model = new BasicReversi();
-    }
+  @Before
+  public void init() {
+    this.model = new BasicReversi();
+  }
 
-    @Test
-    public void testMakeAMove() {
-        this.model.move(new Coordinate(4, 7));
-        Assert.assertEquals(PlayerColor.BLACK, model.getTileAt(new Coordinate(4, 7)).getContents());
-    }
+  @Test
+  public void testMakeAMove() {
+    this.model.move(new Coordinate(4, 7));
+    Assert.assertEquals(PlayerColor.BLACK, model.getTileAt(new Coordinate(4, 7)).getContents());
+  }
 
-    @Test
-    public void testMoveOntoDiscThrowsState() {
-        Assert.assertThrows(IllegalStateException.class,
-                () -> this.model.move(new Coordinate(4, 5)));
-    }
+  @Test
+  public void testMoveOntoDiscThrowsState() {
+    Assert.assertThrows(IllegalStateException.class,
+        () -> this.model.move(new Coordinate(4, 5)));
+  }
 
-    @Test
-    public void testMoveFlipsTiles() {
-        Assert.assertNull(this.model.getTileAt(new Coordinate(4, 7)).getContents());
-        this.model.move(new Coordinate(4, 7));
-        Assert.assertEquals(PlayerColor.BLACK,
-                this.model.getTileAt(new Coordinate(4, 7)).getContents());
-        Assert.assertEquals(PlayerColor.BLACK,
-                this.model.getTileAt(new Coordinate(5, 6)).getContents());
-        Assert.assertEquals(PlayerColor.BLACK,
-                this.model.getTileAt(new Coordinate(6, 5)).getContents());
-    }
+  @Test
+  public void testMoveFlipsTiles() {
+    Assert.assertNull(this.model.getTileAt(new Coordinate(4, 7)).getContents());
+    this.model.move(new Coordinate(4, 7));
+    Assert.assertEquals(PlayerColor.BLACK,
+        this.model.getTileAt(new Coordinate(4, 7)).getContents());
+    Assert.assertEquals(PlayerColor.BLACK,
+        this.model.getTileAt(new Coordinate(5, 6)).getContents());
+    Assert.assertEquals(PlayerColor.BLACK,
+        this.model.getTileAt(new Coordinate(6, 5)).getContents());
+  }
 
-    @Test
-    public void testPassSkipsCurrentPlayer() {
-        Assert.assertEquals(PlayerColor.BLACK, this.model.getCurrentPlayer());
-        this.model.pass();
-        Assert.assertEquals(PlayerColor.WHITE, this.model.getCurrentPlayer());
-    }
+  @Test
+  public void testPassSkipsCurrentPlayer() {
+    Assert.assertEquals(PlayerColor.BLACK, this.model.getCurrentPlayer());
+    this.model.pass();
+    Assert.assertEquals(PlayerColor.WHITE, this.model.getCurrentPlayer());
+  }
 
-    @Test
-    public void testGetPlayerScore() {
-        Assert.assertEquals(3, model.getPlayerScore(PlayerColor.BLACK));
-        Assert.assertEquals(3, model.getPlayerScore(PlayerColor.WHITE));
-        model.move(new Coordinate(4, 7));
-        Assert.assertEquals(5, model.getPlayerScore(PlayerColor.BLACK));
-        Assert.assertEquals(2, model.getPlayerScore(PlayerColor.WHITE));
-    }
+  @Test
+  public void testGetPlayerScore() {
+    Assert.assertEquals(3, model.getPlayerScore(PlayerColor.BLACK));
+    Assert.assertEquals(3, model.getPlayerScore(PlayerColor.WHITE));
+    model.move(new Coordinate(4, 7));
+    Assert.assertEquals(5, model.getPlayerScore(PlayerColor.BLACK));
+    Assert.assertEquals(2, model.getPlayerScore(PlayerColor.WHITE));
+  }
 
-    @Test
-    public void testTwoPassesEndsGame() {
-        Assert.assertFalse(model.isGameOver());
-        model.pass();
-        model.pass();
-        Assert.assertTrue(model.isGameOver());
-    }
+  @Test
+  public void testTwoPassesEndsGame() {
+    Assert.assertFalse(model.isGameOver());
+    model.pass();
+    model.pass();
+    Assert.assertTrue(model.isGameOver());
+  }
 }
